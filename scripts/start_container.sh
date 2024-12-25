404eaec8af2eb10400261d1b55e0ad61bc836035
Kishor Gujar <91127600+Kishorgujar@users.noreply.github.com>
2024-12-25 17:13:59 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,6 +4,7 @@
 REGION="ap-south-1"
 ACCOUNT_ID="796973514691"
 REPOSITORY_NAME="mydemo"
+CONTAINER_NAME="mycontainer"
 
 # Authenticate to ECR using the AWS CLI with non-interactive login
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
@@ -11,8 +12,8 @@ aws ecr get-login-password --region $REGION | docker login --username AWS --pass
 # Pull the Docker image from ECR (using the "latest" tag)
 docker pull $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:latest
 
-# Remove Exist Container
-docker rm -f $CONTAINER_NAME
+# Remove any existing container with the same name, and suppress errors if it doesn't exist
+docker rm -f $CONTAINER_NAME || true
 
 # Run the Docker container (optional, customize as needed)
-docker run -d --name mycontainer $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:latest
+docker run -d --name $CONTAINER_NAME $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:latest
