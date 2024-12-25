48579b7cd1f9447e372e84ace663b610de31129b
Kishor Gujar <91127600+Kishorgujar@users.noreply.github.com>
2024-12-25 16:31:13 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-sudo su
+
 # Set variables
 REGION="ap-south-1"
 ACCOUNT_ID="796973514691"
 REPOSITORY_NAME="mydemo"
 
-# Authenticate to ECR
-sudo aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+# Authenticate to ECR using the AWS CLI with non-interactive login
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 
 # Pull the Docker image from ECR (using the "latest" tag)
-sudo docker pull $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:latest
+docker pull $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:latest
 
 # Run the Docker container (optional, customize as needed)
-sudo docker run -d --name mycontainer $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:latest
-
+docker run -d --name mycontainer $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$REPOSITORY_NAME:latest
